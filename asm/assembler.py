0a8372a2c9574de2665bdb968073aa6d4e9be596
Wagner Macedo <wagnerluis1982@gmail.com>
2013-09-08 01:30:36 -0300

    Defined abstract class Word
    For be inherited by InstructionWord and DataWord

--- asm/assembler.py
+++ asm/assembler.py
@@ -3,43 +3,102 @@
 # NOTE: This was intended to be a parser, but isn't due to lack of knowledge.
 
 import ctypes
+from abc import ABCMeta, abstractmethod
 
 from asm.asm_lexer import lexer
 
 
-class InstructionWord(ctypes.Structure):
-    """Represent the memory instruction word
+class Word(object):
+    __metaclass__ = ABCMeta
 
-        >>> iw = InstructionWord(0b00001, 0b010, 0b00000011)
-        >>> bin(iw.value)
-        '0b101000000011'
-    """
-    _fields_ = [('opcode', ctypes.c_ubyte, 5),
-                ('flags', ctypes.c_ubyte, 3),
-                ('operand', ctypes.c_ubyte, 8)]
+    @abstractmethod
+    def get_value(self):
+        pass
 
-    def __init__(self, opcode=0, flags=0, operand=0, lineno=0):
-        ctypes.Structure.__init__(self, opcode, flags, operand)
-        self.lineno = lineno
+    @abstractmethod
+    def set_value(self, value):
+        pass
 
     @property
     def value(self):
-        return (self.opcode << 3 | self.flags) << 8 | self.operand
-
+        return self.get_value()
     @value.setter
     def value(self, val):
-        self.opcode = val >> 11
-        self.flags = val >> 8
-        self.operand = val
+        self.set_value(val)
+
+    def __eq__(self, o):
+        return self.value == o.value
+
+
+class InstructionWord(Word):
+    '''Represent the memory instruction word'''
+
+    class Container(ctypes.Structure):
+        _fields_ = [('opcode', ctypes.c_ubyte, 5),
+                    ('flags', ctypes.c_ubyte, 3),
+                    ('operand', ctypes.c_ubyte, 8)]
+
+    def __init__(self, opcode=0, flags=0, operand=0, lineno=0, value=None):
+        self._cont = InstructionWord.Container()
+        if value is not None:
+            self.value = value  # setting whole word
+        else:
+            self.opcode = opcode
+            self.flags = flags
+            self.operand = operand
+        # Set an associated assembly code line number
+        self.lineno = lineno
+
+    def get_value(self):
+        return (self.opcode << 3 | self.flags) << 8 | self.operand
+
+    def set_value(self, value):
+        self.opcode = value >> 11
+        self.flags = value >> 8
+        self.operand = value
+
+    @property
+    def opcode(self):
+        return self._cont.opcode
+    @opcode.setter
+    def opcode(self, value):
+        self._cont.opcode = value
+
+    @property
+    def flags(self):
+        return self._cont.flags
+    @flags.setter
+    def flags(self, value):
+        self._cont.flags = value
+
+    @property
+    def operand(self):
+        return self._cont.operand
+    @operand.setter
+    def operand(self, value):
+        self._cont.operand = value
 
     def __repr__(self):
         return 'InstructionWord(%d, %d, %d, lineno=%d)' % (self.opcode,
                                         self.flags, self.operand, self.lineno)
 
 
-class DataWord(ctypes.Structure):
+class DataWord(Word):
     """Represent the memory data word"""
-    _fields_ = [('value', ctypes.c_uint16)]
+
+    class Container(ctypes.Structure):
+        _fields_ = [('data', ctypes.c_uint16)]
+
+
+    def __init__(self, value=0):
+        self._cont = DataWord.Container()
+        self.value = value
+
+    def get_value(self):
+        return self._cont.data
+
+    def set_value(self, value):
+        self._cont.data = value
 
     def __repr__(self):
         return 'DataWord(%d)' % (self.value)
