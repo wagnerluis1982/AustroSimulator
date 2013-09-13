46796e437d174c2d02ef6332bba089704720b834
Wagner Macedo <wagnerluis1982@gmail.com>
2013-09-13 00:53:45 -0300

    Complete tests for MOV instruction

--- tests/test_cycle.py
+++ tests/test_cycle.py
@@ -24,19 +24,93 @@ class ShowRegisters(Step):
         self.messages.append(self.fmt % tuple(args))
 
 
+class ShowMemories(Step):
+    def __init__(self, *numbers):
+        self.messages = []
+        self.fmt = ','.join(['[%d]=%%d' % n for n in numbers])
+        self.indexes = numbers
+
+    def do(self):
+        args = [self.cycle.cpu.memory[i] for i in self.indexes]
+        self.messages.append(self.fmt % tuple(args))
+
+
 class TestMachineCycle(unittest.TestCase):
     '''MachineCycle'''
 
-    def test_mov__reg_const(self):
-        '''mov should load registers with a constant'''
+    def test_mov__reg_reg(self):
+        '''mov should load a register from another register'''
 
         # mov instructions
-        asmd = assemble("""
-            mov ax, 9
-            mov bx, 4
-            mov cx, ax
-            halt
-        """)
+        assembly = ("mov ax, 3\n"
+                    "mov bx, ax\n"
+                    "halt\n")
+        # registers
+        registers = ('AX', 'BX')
+        # expected messages
+        messages = ['AX=0,BX=0',  # start
+                    'AX=3,BX=0',  # after "mov ax, 3"
+                    'AX=3,BX=3',  # after "mov bx, ax"
+                    'AX=3,BX=3']  # after "halt"
+
+        self.register_asserts(assembly, registers, messages)
+
+    def test_mov__reg_const(self):
+        '''mov should load a register from a constant'''
+
+        # mov instructions
+        assembly = ("mov ax, 9\n"
+                    "mov bx, 4\n"
+                    "halt\n")
+        # registers
+        registers = ('AX', 'BX')
+        # expected messages
+        messages = ['AX=0,BX=0',  # start
+                    'AX=9,BX=0',  # after "mov ax, 9"
+                    'AX=9,BX=4',  # after "mov bx, 4"
+                    'AX=9,BX=4']  # after "halt"
+
+        self.register_asserts(assembly, registers, messages)
+
+    def test_mov__reg_mem(self):
+        '''mov should load a register from a memory position'''
+
+        # mov instructions
+        assembly = ("mov ax, 7\n"
+                    "mov [128], ax\n"
+                    "mov bx, [128]\n"
+                    "halt\n")
+        # registers
+        registers = ('BX',)
+        # expected messages
+        messages = ['BX=0',  # start
+                    'BX=0',  # after "mov ax, 7"
+                    'BX=0',  # after "mov [128], ax"
+                    'BX=7',  # after "mov bx, [128]"
+                    'BX=7']  # after "halt"
+
+        self.register_asserts(assembly, registers, messages)
+
+    def test_mov__mem_reg(self):
+        '''mov should load a memory position from a register'''
+
+        # mov instructions
+        assembly = ("mov ax, 5\n"
+                    "mov [128], ax\n"
+                    "halt\n")
+        # memory addresses
+        memories = (128,)
+        # expected messages
+        messages = ['[128]=0',  # start
+                    '[128]=0',  # after "mov ax, 5"
+                    '[128]=5',  # after "mov [128], ax"
+                    '[128]=5']  # after "halt"
+
+        self.memory_asserts(assembly, memories, messages)
+
+    def register_asserts(self, assembly, registers, messages):
+        # mov instructions
+        asmd = assemble(assembly)
 
         cpu = CPU()
         cpu.set_memory_block(asmd['words'])
@@ -44,14 +118,24 @@ class TestMachineCycle(unittest.TestCase):
         # Cycle needs a CPU object
         mach_cycle = MachineCycle(cpu)
         # Test run
-        show = ShowRegisters('AX', 'BX', 'CX')
+        show = ShowRegisters(*registers)
         self.assertTrue( mach_cycle.start(show) )
 
         # Test step
-        self.assertEqual( show.messages,
-                    ['AX=0,BX=0,CX=0',   # start
-                     'AX=9,BX=0,CX=0',   # after "mov ax, 9"
-                     'AX=9,BX=4,CX=0',   # after "mov bx, 4"
-                     'AX=9,BX=4,CX=9',   # after "mov cx, ax"
-                     'AX=9,BX=4,CX=9']   # after "halt"
-                )
+        self.assertEqual( show.messages, messages )
+
+    def memory_asserts(self, assembly, addresses, messages):
+        # mov instructions
+        asmd = assemble(assembly)
+
+        cpu = CPU()
+        cpu.set_memory_block(asmd['words'])
+
+        # Cycle needs a CPU object
+        mach_cycle = MachineCycle(cpu)
+        # Test run
+        show = ShowMemories(*addresses)
+        self.assertTrue( mach_cycle.start(show) )
+
+        # Test step
+        self.assertEqual( show.messages, messages )
