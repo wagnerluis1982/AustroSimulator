a7ec93fcdff0ab420be0ff6d4c38dcd2d053344d
Wagner Macedo <wagnerluis1982@gmail.com>
2013-09-18 23:57:49 -0300

    GUI: step over implemented

--- austro/ui/mainwindow.py
+++ austro/ui/mainwindow.py
@@ -1,12 +1,12 @@
 import os
 
 from PySide.QtGui import *
-from PySide.QtCore import Qt, QSize
+from PySide.QtCore import QEventLoop, Qt, QSize
 from PySide.QtDeclarative import QDeclarativeView
 from PySide.QtUiTools import QUiLoader
 
 from austro.asm import assembler
-from austro.simulator.cpu import CPU
+from austro.simulator.cpu import CPU, Stage, StepEvent
 from austro.ui.codeeditor import CodeEditor, AssemblyHighlighter
 from austro.ui.models import DataModel, RegistersModel, MemoryModel
 
@@ -16,9 +16,27 @@ def _resource(*rsc):
     return os.path.join(directory, *rsc)
 
 
+class ModelsUpdater(StepEvent):
+    def __init__(self, win):
+        self.win = win
+        self.pause = QEventLoop()
+        self.to_pause = False
+
+    def on_fetch(self):
+        # Highlight current execution line
+        lineno = self.cpu.registers.get_word('RI').lineno
+        self.win.asmEdit.highlightLine(lineno)
+
+        self.win.refreshModels()
+        if self.to_pause:
+            self.pause.exec_()
+
+
 class MainWindow(object):
     def __init__(self):
-        self.cpu = CPU()
+        self.event = ModelsUpdater(self)
+
+        self.cpu = CPU(self.event)
         cpu = self.cpu
 
         loader = QUiLoader()
@@ -99,33 +117,67 @@ class MainWindow(object):
         self.actionRun.triggered.connect(self.runAction)
 
         self.actionStep = gui.findChild(QAction, "actionStep")
+        self.actionStep.triggered.connect(self.nextInstruction)
 
         self.actionStop = gui.findChild(QAction, "actionStop")
         self.actionStop.triggered.connect(self.stopAction)
 
     def loadAssembly(self):
+        # Disable editor so will reflect the running program
         editor = self.asmEdit
+        editor.setDisabled()
+
+        # Assemble the program
         assembly = editor.toPlainText()
         asmd = assembler.assemble(assembly)
 
+        # Reset and set the memory with the written program
         self.cpu.reset()
         self.cpu.set_memory_block(asmd['words'])
         self.refreshModels()
 
+        # Enable/Disable actions
         self.actionLoad.setEnabled(False)
         self.actionRun.setEnabled(True)
         self.actionStep.setEnabled(True)
         self.actionStop.setEnabled(True)
 
     def runAction(self):
-        self.cpu.start()
-        self.refreshModels()
+        self.event.to_pause = False
+        if self.cpu.stage == Stage.STOPPED:
+            self.cpu.start()
+        else:
+            self.event.pause.exit()
+
+        self.restoreEditor()
+
+    def nextInstruction(self):
+        if self.cpu.stage == Stage.STOPPED:
+            self.event.to_pause = True
+            self.cpu.start()
+        else:
+            self.event.pause.exit()
+
+        if self.cpu.stage == Stage.HALTED:
+            self.restoreEditor()
 
     def stopAction(self):
+        # Stop correctly
+        self.cpu.stop()
+        if self.event.pause.isRunning():
+            self.event.pause.exit()
+
+        self.restoreEditor()
+
+    def restoreEditor(self):
+        # Enable/Disable actions
         self.actionLoad.setEnabled(True)
         self.actionRun.setEnabled(False)
         self.actionStep.setEnabled(False)
         self.actionStop.setEnabled(False)
+        # Re-enable editor
+        self.asmEdit.setEnabled()
+        self.asmEdit.setFocus()
 
     def refreshModels(self):
         self.genRegsModel.refresh()
